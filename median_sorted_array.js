c9bcac2b6772c11fb8e8c22443625a3bc19cc064
atrocious-jpg <jhnmuinde@gmail.com>
2023-01-07 12:14:52 +0300

    Median of Two Sorted Arrays

--- median_sorted_array.js
+++ median_sorted_array.js
@@ -1,22 +1,29 @@
+/* Median of Two Sorted Arrays
+Hard
+
+Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+The overall run time complexity should be O(log (m+n)). */
+
 var findMedianSortedArrays = function (nums1, nums2) {
   let list = [...nums1, ...nums2].sort((a, b) => a - b);
-//   let i = 0;
-//   let j = 0;
-  //   while (i < nums1.length || j < nums2.length) {
-  //       if (i >= nums1.length) {
-  //           list.push(nums2[j]);
-  //           j++;
-  //       } else if (j >= nums2.length) {
-  //           list.push(nums1[i]);
-  //           i++;
-  //       } else if (nums1[i] < nums2[j]) {
-  //           list.push(nums1[i]);
-  //           i++;
-  //       } else {
-  //           list.push(nums2[j]);
-  //           j++;
-  //       }
-  //   }
+  /*   let i = 0;
+  let j = 0;
+    while (i < nums1.length || j < nums2.length) {
+        if (i >= nums1.length) {
+            list.push(nums2[j]);
+            j++;
+        } else if (j >= nums2.length) {
+            list.push(nums1[i]);
+            i++;
+        } else if (nums1[i] < nums2[j]) {
+            list.push(nums1[i]);
+            i++;
+        } else {
+            list.push(nums2[j]);
+            j++;
+        }
+    } */
   let result =
     list.length % 2 == 0
       ? (list[list.length / 2 - 1] + list[list.length / 2]) / 2
