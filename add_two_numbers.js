c8d981924a0710de34168f0e05b6a0706efbfbd6
atrocious-jpg <jhnmuinde@gmail.com>
2023-01-07 12:30:43 +0300

    Add Two Numbers linked list

--- add_two_numbers.js
+++ add_two_numbers.js
@@ -1,10 +1,9 @@
-class ListNode {
-  constructor(val, next) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
-  }
-}
+/* Add Two Numbers
+Medium
 
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself. */
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -17,34 +16,41 @@ class ListNode {
  * @param {ListNode} l2
  * @return {ListNode}
  */
-// var addTwoNumbers = function (l1, l2) {
-//   let sum = 0;
-//   let current = new ListNode(0);
-//   let result = current;
 
-//   while (l1 || l2) {
-//     if (l1) {
-//       sum += l1.val;
-//       l1 = l1.next;
-//     }
+class ListNode {
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+var addTwoNumbers = function (l1, l2) {
+  let sum = 0;
+  let current = new ListNode(0);
+  let result = current;
 
-//     if (l2) {
-//       sum += l2.val;
-//       l2 = l2.next;
-//     }
+  while (l1 || l2) {
+    if (l1) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
 
-//     current.next = new ListNode(sum % 10);
-//     current = current.next;
+    if (l2) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
 
-//     sum = sum > 9 ? 1 : 0;
-//   }
+    current.next = new ListNode(sum % 10);
+    current = current.next;
 
-//   if (sum) {
-//     current.next = new ListNode(sum);
-//   }
+    sum = sum > 9 ? 1 : 0;
+  }
 
-//   return result.next;
-// };
+  if (sum) {
+    current.next = new ListNode(sum);
+  }
+
+  return result.next;
+};
 
 function addTwoNumbers(l1, l2) {
   let result = new ListNode(0);
